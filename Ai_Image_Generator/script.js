c5413b55777ddb0501cdf896a227e2f3076d0e34
Krishnendu Mahapatra <krishnendumahapatra007@gmail.com>
2025-02-22 12:48:34 +0530

    updated script code

--- Ai_Image_Generator/script.js
+++ Ai_Image_Generator/script.js
@@ -1,6 +1,6 @@
 const generateForm = document.querySelector(".generate-form");
 const imgGallery = document.querySelector(".image-gallery");
-const HUGGINGFACE_API_KEY = "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";
+const HUGGINGFACE_API_KEY = "Enter your API key here";
 let isImageGenerating = false;
 
 const updateImageCard = (imgUrlArray) => {
